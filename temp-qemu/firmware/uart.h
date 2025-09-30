18e56749476c16d1cb7a81f9ae8351e49063e80b
andre <andrea.mugnaini3@stud.unifi.it>
2025-09-30 13:51:38 +0200

    modified uart

--- temp-qemu/firmware/uart.h
+++ temp-qemu/firmware/uart.h
@@ -1,17 +1,15 @@
-#ifndef __UARTH__
-#define __UARTH__
+#ifndef __PRINTF__
+#define __PRINTF__
 
 #include <stdint.h>
 
-#define UART0_ADDRESS (0x40328000UL)
-#define UART0_DATA (*(((volatile uint32_t *)(UART0_ADDRESS + 0UL))))
-#define UART0_STATE (*(((volatile uint32_t *)(UART0_ADDRESS + 0x4UL))))
-#define UART0_CTRL (*(((volatile uint32_t *)(UART0_ADDRESS + 0x8UL))))
-#define UART0_BAUDDIV (*(((volatile uint32_t *)(UART0_ADDRESS + 16UL))))
-#define UART0_FLAGREG (*(((volatile uint32_t *)(UART0_ADDRESS + 0x18UL))))
+#define UART0_ADDRESS                         ( 0x40328000UL )
+#define UART0_DATA                            ( *( ( ( volatile uint32_t * ) ( UART0_ADDRESS + 0UL ) ) ) )
+#define UART0_STATE                           ( *( ( ( volatile uint32_t * ) ( UART0_ADDRESS + 4UL ) ) ) )
+#define UART0_CTRL                            ( *( ( ( volatile uint32_t * ) ( UART0_ADDRESS + 8UL ) ) ) )
+#define UART0_BAUDDIV                         ( *( ( ( volatile uint32_t * ) ( UART0_ADDRESS + 16UL ) ) ) )
 
 void UART_init(void);
-void UART_putc(const char c);
-void UART_printf(const char *fmt, ...);
+void UART_printf(const char *s);
 
 #endif
